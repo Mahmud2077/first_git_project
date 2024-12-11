bdcc8aa556d4bc82408d97eda4b229c40ad631b2
Mahmud2077 <mahmute3541@gmail.com>
2024-12-11 14:19:48 -0500

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -4,7 +4,9 @@ public class HelloCydeo {
     public static void main(String[] args) {
 
         System.out.println("Hello Cydeo");
-        System.out.println("Hello Batch38");
+        System.out.println("Hello Batch38"); 
+        system.out.println("Hello 2025" 
     }
+    
 
 }
